fba3585dbeba57690b310402ef0b2070790bbb81
qurba-mushtaque <122434226+qurba-mushtaque@users.noreply.github.com>
2023-11-30 22:10:55 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include "Airport.hpp"
 #include "Runway.hpp"
+#include "Terminal.hpp"
 #include <iostream>
 #include <map>
 using namespace std;
 
 int main() {
-    cout<<"What do you want to use:\n 1. Airport\n 2.Ruway\n";
+    cout<<"What do you want to use:\n 1. Airport\n 2.Runway\n 3. Terminal\n";
     int choice1;
     cin>>choice1;
     if(choice1==1){
     std::map<int, Airport> airportMapping;
+
     Airport airport;
 
     int choice;
@@ -52,6 +54,23 @@ int main() {
 
 
 
+
+
+    }
+    else if(choice1==3){
+            terminal termialObj;
+            std::map<int, terminal> Terminalmapping;
+            cout<<"What do you want to do?\n";
+            cout<<"\n Menu: \n 1- Add Terminal \n 2- Delete Terminal\n";
+            int TerminalChoice;
+            cin>>TerminalChoice;
+            if(TerminalChoice==1){
+                termialObj.addTerminal(Terminalmapping);
+                
+            }
+            else if(TerminalChoice==2){
+                termialObj.deleteTerminal(Terminalmapping);
+            }
 
 
     }
