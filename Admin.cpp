68daa3b50fd302fb0547f43c4cb5ce000b42e3d6
ff08435 <122521082+ff08435@users.noreply.github.com>
2023-12-04 12:34:43 +0500

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include "Admin.hpp"
+#include <cstring>
+using namespace std;
 
+virtual void addManager(std::map<int, Manager> &ManagerMapping){
+
+    cout << "Please Enter Manager's Name: " << endl;
+    cin >> name;
+    cout << "Please Enter Manager's Username:" << endl;
+    cin >> Username;
+    cout << "Please enter Manager's password: " << endl;
+    cin >> password;
+    ManagerMapping[Username] = *this;
+
+    cout << "Manager Infor added successfully. Username: " << Username << endl;
+
+};
+virtual void deleteManager(std::map<int, Manager> &ManagerMapping){
+    string uName;
+    cout << "Enter the user name of the manager that you wish to delete: " << endl;
+    cin >> uName;
+    std::map<int, Manager>::iterator it = Managermapping.find(uName);
+    if(it !=Managermapping.end()){
+        Managermapping.erase(it);
+        cout<<"Manager info Deleted Succesfully"<<endl;
+}
