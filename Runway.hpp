87dc35c50ffa6d50c9fd02fdef6e701178bbc535
qurba-mushtaque <122434226+qurba-mushtaque@users.noreply.github.com>
2023-12-04 12:49:28 +0500

    Update Runway.hpp

--- Runway.hpp
+++ Runway.hpp
@@ -1,14 +1,19 @@
 #include <iostream>
-#include <Airport.hpp>
+#include <cstring>
+#include <map>
 using namespace std;
 
 class Runway{
     private:
         int RunwayID;
+        int static autoIncRunwayId;
         protected:
         int RunwayNumber;
         double RunwayLength;
     public:
-        void addRunway();
-        void deleteRunway();
+
+        void addRunway(std::map<int, Runway> & Runwaymapping);
+        void deleteRunway(std::map<int, Runway> & Runwaymapping);
+        int generateRunwayId();
+
 };
