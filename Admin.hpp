a79d0434717edc48785992fcb710eef195338fef
Muneeba-Badar <122147598+Muneeba-Badar@users.noreply.github.com>
2023-12-07 22:52:33 +0500

    Add files via upload

--- Admin.hpp
+++ Admin.hpp
@@ -7,6 +7,7 @@ class Admin{
         string username;
         string password;
     public:
-        virtual void addManager(std::map<int, Manager> &ManagerMapping);
-        virtual void deleteManager(std::map<int, Manager> &ManagerMapping);
-        
+        void addManager();
+        void deleteManager();
+        void editManager();
+};
