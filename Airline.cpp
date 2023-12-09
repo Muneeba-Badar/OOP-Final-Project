3dbed1d678de6a65ce4b3134c7271484a35d4237
ff08435 <122521082+ff08435@users.noreply.github.com>
2023-12-09 13:19:54 +0500

    Update Airline.cpp

--- Airline.cpp
+++ Airline.cpp
@@ -4,50 +4,79 @@
 #include "Airline.hpp"
 using namespace std;
 
-int Airline::autoIncId = 0;
+int Airline::autoIncAirlineId = 0;
 
-int Airline:: generateId()
+int Airline:: generateAirlineId()
     {
-        return autoIncId++;
+        return autoIncAirlineId;
     }
 
+void Airline::setConnection(SAConnection* conn){
+    connection = conn;
+}
+void Airline::addAirline(SAConnection* conn){
+    airlineID = generateAirlineID();
 
-void Airline::addAirline(std::map<int, Airline> &airlineMapping)
-{
-    airlineID = generateId();
-    cout << "Enter airline name: ";
-    // cin >> airlineName;
-    getline(cin, airlineName);
-    cout << "Enter Contact Person name: ";
+    cout<<"Enter the Airline Name: ";
+    cin>> airlineName;
+    cout << "Enter the name of The Representative (Contact Person Name): ";
     cin >> contactPersonName;
-    cout << "Enter the phone number of the contact person: ";
-    cin >> phoneNumber;
-    cout << "Enter the email of the contact person: ";
+    cout<<"Enter contact information of the Airline representative ";
+    cin>> phoneNumber;
+    cout << "Enter Email of the Airline company  ";
     cin >> email;
-    cout << "Enter the Headquarter country: ";
-    cin >> HQcountry;
-    cout << "Enter the Headquarter City: ";
-    cin >> HQcity;
+    cout<<"Enter the name of city where Head Quarter is located  ";
+    cin>> HQcity;
+    cout<<"Enter the name of the country the Airline is from ";
+    cin>> HQcountry;
+ 
 
-    // Add the airline to the map
-    airlineMapping[airlineID] = *this;
+    // insert data into the database
+    SACommand cmd(&connection);
+    cmd.setCommandText("INSERT INTO Airline (AirlineID, AirlineName, Contact, Email, HQCity, HQcountry) VALUES (:1, :2, :3, :4, :5, :6)");
+    cmd << airlineID << airlineName << contactPersonName << phoneNumber << email << HQcity  << HQcountry;
+    cmd.Execute();
 
     cout << "Airline added successfully. ID: " << airlineID << endl;
 }
 
 
-void Airline::printAllAirlines(std::map<int, Airline> &airlineMapping)
-{
-    cout << "\nAll Airlines:\n";
-    for (const auto &entry : airlineMapping)
+void Airline:: deleteAirline(SAConnection* conn){
+    int deleteAirlineid;
+        cout << "Enter Id of the Airline you want to delete: ";
+        cin >> deleteAirlineid;
+        
+        // delete data from databse
+        SACommand cmd(&connection);
+        cmd.setCommandText("DELETE FROM Airline WHERE AirlineId = :1");
+        cmd << deleteAirlineid;
+        
+        // checks if data to be deleted is present in the database then delete else produce error message
+        if (cmd.Execute() > 0) {
+            cout << "Airline deleted successfully." << endl;
+        }
+        else {
+            cout << "Airline not found." << endl;
+        }
+}
+
+void Airline::menu(SAConnection* conn){
+        int choice;
+    while (true)
     {
-        const Airline &airline = entry.second;
-        cout << "Airline ID: " << airline.airlineID << "\n"
-             << "Name: " << airline.airlineName << "\n"
-             << "Contact Person: " << airline.contactPersonName << "\n"
-             << "Phone Number: " << airline.phoneNumber << "\n"
-             << "Email: " << airline.email << "\n"
-             << "HQ City: " << airline.HQcity << "\n"
-             << "HQ Country: " << airline.HQcountry << "\n\n";
+        cout<<"What do you want to do?\n";
+        cout<<"\n Menu: \n 1. Add Airline: \n 2. Delete Airline\n 3. Exit\n";
+        cin >> choice;
+        if(choice == 1){
+            addAirline(conn);
+        }
+        else if(choice == 2){
+            deleteAirline(conn);
+        }
+        else{
+            cout << "Invalid choice. Please enter a valid option.\n"
+        }
     }
+}
+
 }
